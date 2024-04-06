ef8bec2402d8bb5525af77c2228e5666a135dbc6
Fernando058 <141414792+Fernando058@users.noreply.github.com>
2024-04-05 21:46:55 -0500

    Add files via upload

---
+++ get_locations.php
@@ -0,0 +1,27 @@
+<?php
+// Conexión a la base de datos
+$servername="bou07gzqrfgzqgcjkzst-mysql.services.clever-cloud.com";
+$username="usvnedkk1cscttv7";
+$password="uscmmz5vOOlksCOD4tKP";
+$dbname="bou07gzqrfgzqgcjkzst";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Consulta para obtener las ubicaciones
+$sql = "SELECT name, description, latitude, longitude FROM locations";
+$result = $conn->query($sql);
+
+$locations = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $locations[] = $row;
+    }
+}
+
+echo json_encode($locations);
+
+$conn->close();
+?>
